8d637343eb562c5b7e334ac428e68149c980bc45
pproe <40435390+pproe@users.noreply.github.com>
2019-10-13 20:43:27 +0800

    assigned motor functions, completed turn functionality, and assigned pin read and writes as per fritzing design

--- AssignmentDraftCode/AssignmentDraftCode.ino
+++ AssignmentDraftCode/AssignmentDraftCode.ino
@@ -2,69 +2,142 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_HMC5883_U.h>
 
-#define COMPASSPIN_SCL 7 //THESE NEED TO CHANGE TO SCL AND SDA PINS
-#define COMPASSPIN_SDA 6 //DONT need to specify pins for the compass readings
-#define PSDPIN 14
-
-#define TURN ~~     //Determine to indicate when boat should begin turning
+// ## THIS SECTION CONTAINS ADJUSTABLE COMPONENTS ##
+float turn = 10;     //Indicate when boat should begin turning
+int delayAmount = 500; //Indicate how long between each update of direction
 
 //Assigning unique ID to sensor
 Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(12345);
 
 float compassValue;
 float heading;
-
+bool turned = false;
 byte PSDValue;
+unsigned long timeOfTurn;
 
-void turning(){
-  
-  mag.getEvent(&event);
-  float compassValue = atan2(event.magnetic.y, event.magnetic.x);
-  
-  while(compassValue != heading){
-    
-    // left motor on, right motor reverse
+//Left Motor Connections
+int enA = 6;
+int in1 = 7;
+int in2 = 4;
 
-    mag.getEvent(&event);
-    float compassValue = atan2(event.magnetic.y, event.magnetic.x);
-  }
-  // reset the in pins for h-bridge
-}
+//Right Motor Connections
+int enB = 5;
+int in3 = 2;
+int in4 = 3;
+
 
 void setup() {
+
+  // set both motors to maximum speed {PWM 0-255}
+  analogWrite(enA, 255); 
+  analogWrite(enB, 255);
+  
+  //Setup of all motor control pins
+  pinMode(enA, OUTPUT);
+  pinMode(enB, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+
+  //Inital State of motors - All off
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
   
   //Initialise the sensor
   mag.begin();
   sensors_event_t event; 
   mag.getEvent(&event);
+  
+  //record initial heading value
   float heading = atan2(event.magnetic.y, event.magnetic.x);
   
-   
 }
 
 void loop() {
-
   
+  //attain new reading from compass
   mag.getEvent(&event);
   float compassValue = atan2(event.magnetic.y, event.magnetic.x);
 
-  if(PSDValue > TURN){
+  //Attain PSD value
+  PSDValue = analogRead(A0);
+  
+  if(PSDValue > turn && !turned){
+    //changes heading value to be opposite direction and initiates turning procedure
     heading += PI;
     if(heading > 2 * PI){
       heading -= 2 * PI;
     }
+    turning();
   }
-  
+
+   if(PSDValue > turn && turned && millis() - timeOfTurn > 3000){  
+    //turns off motor if the boat reaches the second wall it sees
+    //also checks if it has been at least 3000ms since turn
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, LOW);
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, LOW);
+  }
   
   if(compassValue > heading){
     //Turn right motor on, left motor off
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, LOW);
+    digitalWrite(in3, HIGH);
+    digitalWrite(in4, LOW);
   }
+  
   if(compassValue < heading){
     //Turn left motor on, right motor off
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, LOW);   
   }
-  else{
-    //turn both motors on
+  
+  else{ 
+    // turn both motors on
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+    digitalWrite(in3, HIGH);
+    digitalWrite(in4, LOW);
   }
 
-  delay(100);
+  
+  delay(delayAmount);
+}
+
+void turning(){
+  
+  turned = true;
+
+  //attain new compass reading
+  mag.getEvent(&event);
+  float compassValue = atan2(event.magnetic.y, event.magnetic.x);
+  
+  while(compassValue != heading){
+    // left motor on, right motor reverse
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, HIGH);
+
+    //attain new compass reading
+    mag.getEvent(&event);
+    float compassValue = atan2(event.magnetic.y, event.magnetic.x);
+  }
+  
+  //turn motors off
+  digitalWrite(in1, LOW)
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+  
+  //record time of turn to prevent false positive of second wall detection
+  timeOfTurn = millis();
+  
 }
